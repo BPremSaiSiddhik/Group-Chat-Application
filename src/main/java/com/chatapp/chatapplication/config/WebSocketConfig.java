f12fd821002e815a7f6dd01c9654c85d7f18eed9
BPremSaiSiddhik <premsaisid21@gmail.com>
2025-08-11 19:25:45 +0530

    First Commit

---
+++ src/main/java/com/chatapp/chatapplication/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.chatapp.chatapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*") // TODO: Restrict in production
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<org.springframework.messaging.converter.MessageConverter> messageConverters) {
+        messageConverters.add(new MappingJackson2MessageConverter());
+        return true; // Indicate that default converters should still be included
+    }
+}
